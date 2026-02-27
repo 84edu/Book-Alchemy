da045afd6c54138aa81b1494061746af081cfeaf
Behrooz Mohandeszadeh <behrooz@mohandeszadeh.com>
2026-02-27 07:39:31 +0100

    Implement SQLAlchemy models for Authors and Books and auto-initialize database

---
+++ data_models.py
@@ -0,0 +1,27 @@
+from flask_sqlalchemy import SQLAlchemy
+
+db = SQLAlchemy()
+
+class Author(db.Model):
+    __tablename__ = "authors"
+
+    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    name = db.Column(db.String)
+    birth_date = db.Column(db.Date)
+    date_of_death = db.Column(db.Date)
+
+    def __repr__(self):
+        return f"<Author {self.name}>"
+
+
+class Book(db.Model):
+    __tablename__ = "books"
+
+    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    isbn = db.Column(db.String)
+    title = db.Column(db.String)
+    publication_year = db.Column(db.Integer)
+    author_id = db.Column(db.Integer, db.ForeignKey("authors.id"))
+
+    def __repr__(self):
+        return f"<Book {self.title} (ISBN: {self.isbn})>"
