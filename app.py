bfa32a1ec9bc3f8f7cf8727919f2766cc8e05534
Behrooz Mohandeszadeh <behrooz@mohandeszadeh.com>
2026-02-27 14:56:30 +0100

    Add routes and templates for authors and books
    - Created /add_author route and template
    - Created /add_book route and template
    - Implemented form to select author via dropdown

--- app.py
+++ app.py
@@ -1,9 +1,15 @@
-from flask import Flask
+from docutils.nodes import authors
+from flask import Flask, flash, redirect, render_template, request, url_for
 from flask_sqlalchemy import SQLAlchemy
 import os
 from data_models import db, Author, Book
+from datetime import datetime
+from dotenv import load_dotenv
 
 app = Flask(__name__)
+
+load_dotenv()
+app.secret_key = os.getenv('APP_KEY')
 
 basedir = os.path.abspath(os.path.dirname(__file__))
 app.config['SQLALCHEMY_DATABASE_URI'] = \
@@ -11,5 +17,78 @@ app.config['SQLALCHEMY_DATABASE_URI'] = \
 
 db.init_app(app)
 
-with app.app_context():
-    db.create_all()
+# with app.app_context():
+#     db.create_all()
+
+@app.route('/add_author', methods=['GET', 'POST'])
+def add_author():
+    if request.method == "POST":
+        name = request.form["name"]
+        birthdate_string = request.form["birthdate"]
+        birthdate = datetime.strptime(birthdate_string, "%Y-%m-%d").date()
+
+        date_of_death_string = request.form["date_of_death"]
+        date_of_death = None
+        if date_of_death_string:
+            date_of_death = datetime.strptime(date_of_death_string,
+                                              "%Y-%m-%d").date()
+
+        new_author = Author(
+            name=name,
+            birth_date=birthdate,
+            date_of_death=date_of_death
+        )
+
+        db.session.add(new_author)
+        db.session.commit()
+
+        flash(f"Author {name} added successfully!")
+        return redirect(url_for("add_author"))
+
+    return render_template("add_author.html")
+
+
+@app.route('/add_book', methods=['GET', 'POST'])
+def add_book():
+    if request.method == "GET":
+        authors = Author.query.all()
+        return render_template("add_book.html", authors=authors)
+
+    if request.method == "POST":
+        title = request.form["title"]
+        isbn = request.form["isbn"]
+        publication_year = request.form["publication_year"]
+
+        author_id = int(request.form["author"])
+
+        new_book = Book(
+            title=title,
+            isbn=isbn,
+            publication_year=publication_year,
+            author_id=author_id
+        )
+
+        db.session.add(new_book)
+        db.session.commit()
+
+        flash(f"Book {title} added successfully!")
+
+        return redirect(url_for("add_book"))
+
+
+@app.route('/')
+def home():
+    sort_by = request.args.get('sort', 'title')
+
+    if sort_by == 'name':
+        books = Book.query.join(Author).order_by(Author.name).all()
+    elif sort_by == 'year':
+        books = Book.query.order_by(Book.publication_year).all()
+    else:
+        books = Book.query.order_by(Book.title).all()
+
+    return render_template("home.html", books=books, current_sort=sort_by)
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
--- data_models.py
+++ data_models.py
@@ -9,6 +9,7 @@ class Author(db.Model):
     name = db.Column(db.String)
     birth_date = db.Column(db.Date)
     date_of_death = db.Column(db.Date)
+    books = db.relationship('Book', backref='author', lazy=True)
 
     def __repr__(self):
         return f"<Author {self.name}>"
